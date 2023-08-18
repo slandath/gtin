b68944f29593d2fb2e7506b3abcfd929586ad379
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2023-08-17 23:28:24 -0400

    more stuff

--- app.js
+++ app.js
@@ -1,73 +1,12 @@
-document.getElementById("btnForm").addEventListener("click", function () {
-  calcCheckDigit(
-    "gtinInput",
-    "resultDiv",
-    "visible",
-    "gtinResultDiv",
-    "visible",
-    "checkDigit",
-    "gtin"
-  );
-});
+const degrees = document.querySelector('#degrees');
+const finalTemp = document.querySelector('#finalTemp');
 
-const checkDigitArray = [];
+degrees.addEventListener('input', calc);
 
-function activateDiv(a, b) {
-  document.getElementById(a).className = b;
+function calc() {
+  const foo = 'Celsius';
+  const temperature = (degrees.value - 32) * 0.5556;
+  finalTemp.innerText = `${Math.round(temperature)}\u00B0 ${foo}`;
 }
 
-function checkDigit(a) {
-  checkDigitArray.length = 0;
-  const oddArray = [];
-  const evenArray = [];
-
-  const newArray = a.split("");
-
-  for (let i = 0; i < newArray.length; i += 2) {
-    let b = parseInt(newArray[i]);
-    oddArray.push(b);
-  }
-
-  for (let i = 1; i < newArray.length; i += 2) {
-    let c = parseInt(newArray[i]);
-    evenArray.push(c);
-  }
-
-  let d = 0;
-
-  oddArray.forEach(sumArray);
-  function sumArray(num) {
-    d += num;
-  }
-
-  let e = 0;
-  evenArray.forEach(addArray);
-  function addArray(num) {
-    e += num;
-  }
-
-  let f = d * 3;
-  let g = e + f;
-  let h = g % 10;
-  let j = 10 - h;
-  console.log(d, e, f, g, h, j);
-  if ((j === 0) | (j === 10)) {
-    j = 0;
-  } else {
-    j;
-  }
-
-  checkDigitArray.push(j);
-}
-
-function calcCheckDigit(a, b, c, d, e, f, g) {
-  let x = document.getElementById(a).value;
-  checkDigit(x);
-
-  activateDiv(b, c);
-  activateDiv(d, e);
-
-  document.getElementById(f).innerText = checkDigitArray[0];
-  document.getElementById(g).innerText = x + checkDigitArray[0];
-  event.preventDefault();
-}
+calc();
